1602d28394a53aa698c53169ba35f5de9a2cbd39
tduzan <tyler.duzan@rackspace.com>
2013-08-18 01:45:59 -0500

    added constraints for foreign keys, primary keys, and uniqueness

--- docs/sslparty_ddl.sql
+++ docs/sslparty_ddl.sql
@@ -1,12 +1,12 @@
--- This DDL creates the basic strcuture for sslparty
-CREATEDB -E UTF8 sslparty;
+-- This DDL creates the basic structure for sslparty
+CREATE DATABASE sslparty WITH ENCODING UTF8;
 USE sslparty;
+
 -- Create "groups" table
-CREATE TABLE IF NOT EXISTS groups (id bigserial, group_name varchar(500), group_owner varchar(500), group_email varchar(500));
--- Create "domains" table
-CREATE TABLE IF NOT EXISTS domains (id bigserial, domain_name varchar(256), lastcheck_date timestamp, is_valid boolean, expiry_date date, registrar_name varchar(500), val_ns1 varchar(500), val_ns2 varchar(500), val_ns3 varchar(500), group_id bigint);
--- Create "sites" table
-CREATE TABLE IF NOT EXISTS sites (id bigserial, url varchar(300), port smallint, domain_id bigint, lastcheck_date timestamp, is_valid boolean, expiry_date date, issuer_name varchar(500), serial_number(500), sha1_fingerprint(500), group_id bigint);
+CREATE TABLE IF NOT EXISTS groups (id bigserial PRIMARY KEY, group_name varchar(500) UNIQUE NOT NULL, group_owner varchar(500) NOT NULL, group_email varchar(500) NOT NULL);
 
--- Create indexes
+-- Create "domains" table, constrains on groups.id 
+CREATE TABLE IF NOT EXISTS domains (id bigserial PRIMARY KEY, domain_name varchar(256) UNIQUE NOT NULL, lastcheck_date timestamp NULL, is_valid boolean NULL, expiry_date date NULL, registrar_name varchar(500) NULL, val_ns1 varchar(500) NULL, val_ns2 varchar(500) NULL, val_ns3 varchar(500) NULL, group_id bigint REFERENCES groups (id));
 
+-- Create "sites" table, constrains on groups.id and domains.id
+CREATE TABLE IF NOT EXISTS sites (id bigserial, url varchar(300), port smallint, domain_id bigint REFERENCES domains (id), lastcheck_date timestamp NULL, is_valid boolean NULL, expiry_date date NULL, issuer_name varchar(500) NULL, serial_number(500) NULL, sha1_fingerprint(500) NULL, group_id bigint REFERENCES groups (id), PRIMARY KEY(id, url, port));
